d0f11ca9d7853b1875080965973323dbab5e2ba4
DenisAndreiC <deniscucu0504@gmail.com>
2026-01-13 01:43:56 +0200

    final project cleanup and documentation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:3000").trim(),
-  // withCredentials: true,
+  baseURL: "http://localhost:3000",
+  //withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
